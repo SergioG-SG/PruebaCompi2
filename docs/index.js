16071db779f0da00b2ae434449956151676bb76c
S;G <53422092+SergioG-SG@users.noreply.github.com>
2021-06-09 10:43:04 -0600

    ffff

--- docs/index.js
+++ docs/index.js
@@ -1,5 +1,5 @@
 "use strict";
-import { parse } from './gramaticaXML';
+import { parse } from './gramaticaXML.js';
 function ejecutarXML(entrada) {
     //Parseo para obtener la raiz o raices  
     const objetos = parse(entrada);
